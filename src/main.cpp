713077a8182455eb07a4e96a57eeb149ee465ccc
Alexander Hemberg <alexander.hemberg@gmail.com>
2018-03-13 23:14:29 +0100

    Added methods for reading thermistors.
    NOTE: A bug in the readout causes temperature measurements to be
    off by a factor 10.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,34 @@
 #define AVGS 10
 #define CHARGE_PIN 9
 
+struct ntc_info
+{
+   int pin;
+   float A;
+   float B;
+   float C;
+   float r1;
+};
+
+//typedef struct ntc_info NtcInfo;
+
+ntc_info batt_thermistor = {
+  A2,
+  0.5522033026e-3,
+  3.378163036e-4,
+  -3.876640607e-7,
+  10e3
+};
+
+ntc_info amb_thermistor = {
+  A3,
+  0.6944098729e-3,
+  3.124809880e-4,
+  -2.784875147e-7,
+  10e3
+};
+
+
 int duty = 0;
 float tot_voltage, charge_voltage, batt_voltage, current, set_voltage;
 float charge_current;
@@ -16,9 +44,26 @@ unsigned long last_volt_measurement, loop_top;
 float kp = 2.0, ki = 0.000005, kd = 4;
 PidRegulator spid = PidRegulator(kp, ki, kd);
 
+float temp_from_r(float resistance, float a, float b, float c) {
+  return 1 / (a + b*log(resistance) + c*pow(log(resistance),3));
+}
+
+float ntc_resistance(float vout, float r1) {
+  return (vout*r1)/(5.0 - vout);
+}
+
+float read_ntc_temp(ntc_info thermistor) {
+  float v_meas = (analogRead(thermistor.pin)*5.0)/1023.0;
+  float r_ntc = ntc_resistance(v_meas, thermistor.r1);
+  return temp_from_r(r_ntc, thermistor.A, thermistor.B, thermistor.C);
+}
+
 void setup() {
     pinMode(A0, INPUT);
     pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+
     pinMode(CHARGE_PIN, OUTPUT);
     analogWrite(9, duty);
     batt_voltage = analogRead(A1);
@@ -90,9 +135,12 @@ void loop() {
     //Serial.print(",");
     //Serial.print(output);
     //Serial.print(",");
-    Serial.print(batt_voltage);
+    //Serial.print(batt_voltage);
+    //Serial.print(",");
+    //Serial.print(charge_voltage);
+    Serial.print(read_ntc_temp(batt_thermistor));
     Serial.print(",");
-    Serial.print(charge_voltage);
+    Serial.print(read_ntc_temp(amb_thermistor));
     Serial.println();
 
 
